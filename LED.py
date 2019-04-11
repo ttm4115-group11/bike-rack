5ce3f0a1ffbef185baefeee08f863c0d891347ea
haavard@vaage.com <haavard@vaage.com>
2019-04-11 12:59:44 +0200

    Move pins and timers to setup varialbes, add setup method

--- LED.py
+++ LED.py
@@ -1,55 +1,65 @@
 import RPi.GPIO as GPIO
 import time
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setwarnings(False)
-    
+
+
+
+# Variables
+RED_PIN = 21
+GREEN_PIN = 20
+YELLOW_PIN = 16
+LOCK_PIN = 19
+UNLOCK_PIN = 26
+pins = [RED_PIN, GREEN_PIN, YELLOW_PIN, LOCK_PIN, UNLOCK_PIN]
+
+LED_DURATION = 3  # Seconds
+LOCK_DURATION = 0.5  # Seconds
+
+
+def setup():
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setwarnings(False)
+
+    # Initialize pins
+    for pin in pins:
+        GPIO.setup(pin, GPIO.OUT)
+
 
 def red():
-    GPIO.setup(21, GPIO.OUT)
-    print("LED on")
-    GPIO.output(21, GPIO.HIGH)
-    time.sleep(3)
-    print("LED off")
-    GPIO.output(21, GPIO.LOW)
+    print("RED LED ON")
+    GPIO.output(RED_PIN, GPIO.HIGH)
+    time.sleep(LED_DURATION)
+    print("RED LED OFF")
+    GPIO.output(RED_PIN, GPIO.LOW)
 
 
 def green():
-    GPIO.setup(20, GPIO.OUT)
-    print("LED on")
-    GPIO.output(20, GPIO.HIGH)
-    time.sleep(3)
-    print("LED off")
-    GPIO.output(20, GPIO.LOW)
+    print("GREEN LED ON")
+    GPIO.output(GREEN_PIN, GPIO.HIGH)
+    time.sleep(LED_DURATION)
+    print("GREEN LED OFF")
+    GPIO.output(GREEN_PIN, GPIO.LOW)
         
 
 def yellow():
-    GPIO.setup(16, GPIO.OUT)
-    print("LED on")
-    GPIO.output(16, GPIO.HIGH)
-    time.sleep(3)
+    print("YELLOW PIN ON")
+    GPIO.output(YELLOW_PIN, GPIO.HIGH)
+    time.sleep(LED_DURATION)
     print("LED off")
-    GPIO.output(16, GPIO.LOW)
+    GPIO.output(YELLOW_PIN, GPIO.LOW)
 
 
 # GPIO 19
 def lock():
-    GPIO.setup(19, GPIO.OUT)
-    GPIO.output(19, GPIO.HIGH)
+    GPIO.output(LOCK_PIN, GPIO.HIGH)
     print("LOCK")
-    time.sleep(0.5)
-    GPIO.output(19, GPIO.LOW)
+    time.sleep(LOCK_DURATION)
+    GPIO.output(LOCK_PIN, GPIO.LOW)
 
 
 # GPIO 26
 def unlock():
-    GPIO.setup(26, GPIO.OUT)
-    GPIO.output(26, GPIO.HIGH)
+    GPIO.output(UNLOCK_PIN, GPIO.HIGH)
     print("UNLOCK")
-    time.sleep(0.5)
-    GPIO.output(26, GPIO.LOW)
-
-
-red()
-yellow()
-green()
+    time.sleep(LOCK_PIN)
+    GPIO.output(UNLOCK_PIN, GPIO.LOW)
